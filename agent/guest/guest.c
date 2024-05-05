926049eb99303b996539ebcc8aee80b9f94b92a1
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-05-05 13:15:15 -0400

    x

--- agent/guest/guest.c
+++ agent/guest/guest.c
@@ -1,199 +1,43 @@
-#define _GNU_SOURCE
-
-#include <errno.h>
-#include <fcntl.h>
-#include <poll.h>
-#include <signal.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/signalfd.h>
-#include <unistd.h>
+#include "utils.h"
 
 /*
  * Project Constants
  */
 
-// workspace directory in guest
-#define PATH_WKS "/tmp/wks"
+#ifdef HARNESS
+#define PATH_HARNESS "/home/harness"
 
-// host-guest synchronization mark
-#define FILE_MARK "MARK"
-
-/*
- * inotify-based Synchronization Utility
- */
-
-// dnotify notification signal
-#define DNOTIFY_SIGNAL (SIGRTMIN + 1)
-
-// enumerate list of FDs to poll
-enum { FD_POLL_SIGNAL = 0, FD_POLL_MAX };
-
-static inline int __inotify_initialize_signals(void) {
-  int signal_fd;
-  sigset_t sigmask;
-
-  // handle SIGINT, SIGTERM and SIGRTMIN+1 in the signal_fd
-  sigemptyset(&sigmask);
-  sigaddset(&sigmask, SIGINT);
-  sigaddset(&sigmask, SIGTERM);
-  sigaddset(&sigmask, DNOTIFY_SIGNAL);
-
-  if (sigprocmask(SIG_BLOCK, &sigmask, NULL) < 0) {
-    fprintf(stderr, "cannot block signals: '%s'\n", strerror(errno));
-    return -1;
-  }
-
-  // get new FD to read signals from it
-  if ((signal_fd = signalfd(-1, &sigmask, 0)) < 0) {
-    fprintf(stderr, "cannot setup signal fd: '%s'\n", strerror(errno));
-    return -1;
-  }
-
-  return signal_fd;
-}
-
-static inline int __dnotify_initialize(const char *path, int mask) {
-  int fd = open(path, O_RDONLY);
-  if (fd < 0) {
-    fprintf(stderr, "cannot open directory %s: %s\n", path, strerror(errno));
-    return -1;
-  }
-
-  // init directory notifications using DNOTIFY_SIGNAL
-  if (fcntl(fd, F_SETSIG, DNOTIFY_SIGNAL) < 0 ||
-      fcntl(fd, F_NOTIFY, mask) < 0) {
-    fprintf(stderr, "cannot setup directory notifications in %s: %s\n", path,
-            strerror(errno));
-    close(fd);
-    return -1;
-  }
-
-  return fd;
-}
-
-// block until a specific file is created or deleted in the watched directory
-static inline int dnotify_watch(const char *dir, const char *file,
-                                bool expect_create) {
-  int signal_fd, dir_fd;
-  struct pollfd fds[FD_POLL_MAX];
-
-  // setup
-  if ((signal_fd = __inotify_initialize_signals()) < 0) {
-    return -1;
-  }
-  if ((dir_fd = __dnotify_initialize(dir, expect_create ? DN_CREATE
-                                                        : DN_DELETE)) < 0) {
-    close(signal_fd);
-    return -1;
-  }
-
-  // check existence, skip waiting if existence (or nonexistence) is expected
-  bool exists = faccessat(dir_fd, file, F_OK, 0) == 0;
-  if (expect_create == exists) {
-    goto shutdown;
-  }
-
-  // setup polling
-  fds[FD_POLL_SIGNAL].fd = signal_fd;
-  fds[FD_POLL_SIGNAL].events = POLLIN;
-
-  // block until there is something to read
-  if (poll(fds, FD_POLL_MAX, -1) < 0) {
-    fprintf(stderr, "cannot poll(): '%s'\n", strerror(errno));
-    goto exception;
-  }
-
-  // signal received?
-  if (fds[FD_POLL_SIGNAL].revents & POLLIN) {
-    struct signalfd_siginfo fdsi;
-
-    if (read(fds[FD_POLL_SIGNAL].fd, &fdsi, sizeof(fdsi)) != sizeof(fdsi)) {
-      fprintf(stderr, "cannot read signal, wrong size read\n");
-      goto exception;
-    }
-
-    // break loop if we got the expected signal
-    if (fdsi.ssi_signo == SIGINT || fdsi.ssi_signo == SIGTERM) {
-      fprintf(stderr, "terminating by signal\n");
-      exit(1);
-    }
-
-    // process event if dnotify signal received
-    if (fdsi.ssi_signo == DNOTIFY_SIGNAL) {
-      if (fdsi.ssi_fd != dir_fd) {
-        fprintf(stderr, "unexpect dnotify event received\n");
-        goto exception;
-      }
-
-      bool exists_on_event = faccessat(dir_fd, file, F_OK, 0) == 0;
-      if (expect_create == exists_on_event) {
-        goto shutdown;
-      } else {
-        goto exception;
-      }
-    }
-
-    // other signals are not expected
-    fprintf(stderr, "received unexpected signal\n");
-    goto exception;
-  }
-
-  // shutdown
-shutdown:
-  close(dir_fd);
-  close(signal_fd);
-  return 0;
-
-  // exception
-exception:
-  close(dir_fd);
-  close(signal_fd);
-  return -1;
-}
+#ifdef BLOB
+#define PATH_BLOB "/home/blob"
+#else
+#define PATH_SHELL "/bin/sh"
+#endif
+#endif
 
 /*
  * Entrypoint
  */
 
 int main(int argc, char *argv[]) {
-  fprintf(stderr, "!!!!!!!! [Here in GUEST VM AGENT] !!!!!!!!\n");
-
   // sanity check
   if (argc != 1) {
-    fprintf(stderr, "expect one and only one argument\n");
+    fprintf(stderr, "unexpected arguments for %s\n", argv[0]);
     exit(1);
   }
 
-  // remove the mark to signal that the guest is ready
-  int dir_fd;
-  if ((dir_fd = open(PATH_WKS, O_DIRECTORY | O_RDONLY)) < 0) {
-    fprintf(stderr, "unable to open directory %s: %s\n", PATH_WKS,
-            strerror(errno));
-    exit(1);
-  }
-
-  if (unlinkat(dir_fd, FILE_MARK, 0) != 0) {
-    fprintf(stderr, "failed to unlink %s in %s: %s\n", FILE_MARK, PATH_WKS,
-            strerror(errno));
-    close(dir_fd);
-    exit(1);
-  }
-  if (syncfs(dir_fd) != 0) {
-    fprintf(stderr, "failed to to fsync directory %s: %s\n", PATH_WKS,
-            strerror(errno));
-    close(dir_fd);
-    exit(1);
-  }
-  close(dir_fd);
-
+  // TODO
   fprintf(stderr, "[harness-fuzz] notified host on VM ready\n");
 
-  // wait for mark to re-appear
-  dnotify_watch(PATH_WKS, FILE_MARK, true);
-
-  // transfer control to harness
-  return execl(HARNESS, HARNESS, argv[1]);
+#ifdef HARNESS
+#ifdef BLOB
+  // testing mode
+  return execl(PATH_HARNESS, PATH_HARNESS, PATH_BLOB);
+#else
+  // fuzzing mode (TODO)
+  return execl(PATH_HARNESS, PATH_HARNESS);
+#endif
+#else
+  // shell mode
+  return execl(PATH_SHELL, PATH_SHELL)
+#endif
 }
---
+++ agent/guest/utils.h
@@ -0,0 +1,150 @@
+#ifndef GUEST_UTILS_H
+#define GUEST_UTILS_H
+
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/signalfd.h>
+#include <unistd.h>
+
+/*
+ * inotify-based Synchronization Utility
+ */
+
+// dnotify notification signal
+#define DNOTIFY_SIGNAL (SIGRTMIN + 1)
+
+// enumerate list of FDs to poll
+enum { FD_POLL_SIGNAL = 0, FD_POLL_MAX };
+
+static inline int __inotify_initialize_signals(void) {
+  int signal_fd;
+  sigset_t sigmask;
+
+  // handle SIGINT, SIGTERM and SIGRTMIN+1 in the signal_fd
+  sigemptyset(&sigmask);
+  sigaddset(&sigmask, SIGINT);
+  sigaddset(&sigmask, SIGTERM);
+  sigaddset(&sigmask, DNOTIFY_SIGNAL);
+
+  if (sigprocmask(SIG_BLOCK, &sigmask, NULL) < 0) {
+    fprintf(stderr, "cannot block signals: '%s'\n", strerror(errno));
+    return -1;
+  }
+
+  // get new FD to read signals from it
+  if ((signal_fd = signalfd(-1, &sigmask, 0)) < 0) {
+    fprintf(stderr, "cannot setup signal fd: '%s'\n", strerror(errno));
+    return -1;
+  }
+
+  return signal_fd;
+}
+
+static inline int __dnotify_initialize(const char *path, int mask) {
+  int fd = open(path, O_RDONLY);
+  if (fd < 0) {
+    fprintf(stderr, "cannot open directory %s: %s\n", path, strerror(errno));
+    return -1;
+  }
+
+  // init directory notifications using DNOTIFY_SIGNAL
+  if (fcntl(fd, F_SETSIG, DNOTIFY_SIGNAL) < 0 ||
+      fcntl(fd, F_NOTIFY, mask) < 0) {
+    fprintf(stderr, "cannot setup directory notifications in %s: %s\n", path,
+            strerror(errno));
+    close(fd);
+    return -1;
+  }
+
+  return fd;
+}
+
+// block until a specific file is created or deleted in the watched directory
+static inline int dnotify_watch(const char *dir, const char *file,
+                                bool expect_create) {
+  int signal_fd, dir_fd;
+  struct pollfd fds[FD_POLL_MAX];
+
+  // setup
+  if ((signal_fd = __inotify_initialize_signals()) < 0) {
+    return -1;
+  }
+  if ((dir_fd = __dnotify_initialize(dir, expect_create ? DN_CREATE
+                                                        : DN_DELETE)) < 0) {
+    close(signal_fd);
+    return -1;
+  }
+
+  // check existence, skip waiting if existence (or nonexistence) is expected
+  bool exists = faccessat(dir_fd, file, F_OK, 0) == 0;
+  if (expect_create == exists) {
+    goto shutdown;
+  }
+
+  // setup polling
+  fds[FD_POLL_SIGNAL].fd = signal_fd;
+  fds[FD_POLL_SIGNAL].events = POLLIN;
+
+  // block until there is something to read
+  if (poll(fds, FD_POLL_MAX, -1) < 0) {
+    fprintf(stderr, "cannot poll(): '%s'\n", strerror(errno));
+    goto exception;
+  }
+
+  // signal received?
+  if (fds[FD_POLL_SIGNAL].revents & POLLIN) {
+    struct signalfd_siginfo fdsi;
+
+    if (read(fds[FD_POLL_SIGNAL].fd, &fdsi, sizeof(fdsi)) != sizeof(fdsi)) {
+      fprintf(stderr, "cannot read signal, wrong size read\n");
+      goto exception;
+    }
+
+    // break loop if we got the expected signal
+    if (fdsi.ssi_signo == SIGINT || fdsi.ssi_signo == SIGTERM) {
+      fprintf(stderr, "terminating by signal\n");
+      exit(1);
+    }
+
+    // process event if dnotify signal received
+    if (fdsi.ssi_signo == DNOTIFY_SIGNAL) {
+      if (fdsi.ssi_fd != dir_fd) {
+        fprintf(stderr, "unexpect dnotify event received\n");
+        goto exception;
+      }
+
+      bool exists_on_event = faccessat(dir_fd, file, F_OK, 0) == 0;
+      if (expect_create == exists_on_event) {
+        goto shutdown;
+      } else {
+        goto exception;
+      }
+    }
+
+    // other signals are not expected
+    fprintf(stderr, "received unexpected signal\n");
+    goto exception;
+  }
+
+  // shutdown
+shutdown:
+  close(dir_fd);
+  close(signal_fd);
+  return 0;
+
+  // exception
+exception:
+  close(dir_fd);
+  close(signal_fd);
+  return -1;
+}
+
+#endif // GUEST_UTILS_H
