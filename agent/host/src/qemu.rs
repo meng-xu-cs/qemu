4184afabef0b13e61ef841130f9d0f72a73630d1
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-05-09 22:33:29 -0400

    x

--- agent/host/src/qemu.rs
+++ agent/host/src/qemu.rs
@@ -1,127 +1,24 @@
-use std::fmt::Debug;
 use std::io;
+use std::io::BufReader;
 use std::os::unix::net::UnixStream;
-use std::path::{Path, PathBuf};
-use std::thread::sleep;
-use std::time::Duration;
 
-use log::error;
-use qapi::qmp::{Event, JobStatus, RunState};
-use qapi::{qmp, Command, Qmp};
+use qapi::qmp::{Event, JobStatus};
+use qapi::{qmp, Qmp, Stream};
 
 const SNAPSHOT_TAG: &str = "qce";
 const SNAPSHOT_JOB_PREFIX: &str = "qce_job_";
 const SNAPSHOT_DISK: &str = "disk0";
 
-fn exec_sync<C: Command + Debug>(socket: &Path, command: C) -> io::Result<(C::Ok, Option<Event>)> {
-    let stream = UnixStream::connect(socket)?;
-    let mut qmp = Qmp::from_stream(&stream);
-    qmp.handshake()?;
-
-    // send the command
-    let resp = qmp.execute(&command)?;
-
-    // consume generated events
-    qmp.nop()?;
-    let mut iter = qmp.events();
-    match iter.next() {
-        None => Ok((resp, None)),
-        Some(event) => match iter.next() {
-            None => Ok((resp, Some(event))),
-            Some(e) => {
-                error!("unexpected event {:?}", e);
-                for e in iter {
-                    error!("unexpected event {:?}", e);
-                }
-                Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "more than one events received for a command in sync mode".to_string(),
-                ))
-            }
-        },
-    }
-}
-
-fn exec_async<C: Command>(socket: &Path, command: C, job_id: &str) -> io::Result<()> {
-    let stream = UnixStream::connect(socket)?;
-    let mut qmp = Qmp::from_stream(&stream);
-    qmp.handshake()?;
-
-    // send the command
-    qmp.execute(&command)?;
-
-    // wait for result
-    let mut aborted = false;
-    'outer: loop {
-        qmp.nop()?;
-        for event in qmp.events() {
-            if let Event::JOB_STATUS_CHANGE { data, timestamp: _ } = event {
-                if data.id != job_id {
-                    continue;
-                }
-                match data.status {
-                    JobStatus::created
-                    | JobStatus::ready
-                    | JobStatus::running
-                    | JobStatus::waiting
-                    | JobStatus::pending => (),
-                    JobStatus::aborting => {
-                        aborted = true;
-                    }
-                    JobStatus::concluded => break 'outer,
-                    JobStatus::standby
-                    | JobStatus::null
-                    | JobStatus::undefined
-                    | JobStatus::paused => {
-                        return Err(io::Error::new(
-                            io::ErrorKind::Other,
-                            format!("unexpected job status: {:?}", data.status),
-                        ));
-                    }
-                }
-            }
-        }
-        sleep(Duration::from_millis(1));
-    }
-
-    // early return if job completed successfully
-    if !aborted {
-        return Ok(());
-    }
-
-    // probe for a reason
-    let mut reason = None;
-    for item in qmp.execute(&qmp::query_jobs {})? {
-        if item.id != job_id {
-            continue;
-        }
-        let error = item
-            .error
-            .unwrap_or_else(|| "aborted without an error message".to_string());
-        reason = Some(error);
-        break;
-    }
-
-    Err(io::Error::new(
-        io::ErrorKind::Other,
-        format!(
-            "job aborted: {}",
-            reason.unwrap_or_else(|| "unable to probe for a reason".to_string())
-        ),
-    ))
-}
-
-pub struct QemuProxy {
-    socket: PathBuf,
+pub struct QemuProxy<'a> {
+    qmp: Qmp<Stream<BufReader<&'a UnixStream>, &'a UnixStream>>,
     job_count: usize,
 }
 
-impl QemuProxy {
-    pub fn new(socket: PathBuf) -> Self {
-        Self {
-            socket,
-            job_count: 0,
-        }
+impl<'a> QemuProxy<'a> {
+    pub fn new(stream: &'a UnixStream) -> io::Result<Self> {
+        let mut qmp = Qmp::from_stream(stream);
+        qmp.handshake()?;
+        Ok(Self { qmp, job_count: 0 })
     }
 
     fn next_job_id(&mut self) -> String {
@@ -130,89 +27,152 @@ impl QemuProxy {
         job_id
     }
 
-    fn vm_stop(&mut self) -> io::Result<()> {
-        let (_, event) = exec_sync(&self.socket, qmp::stop {})?;
-        match event {
-            Some(Event::STOP { .. }) => Ok(()),
-            e => Err(io::Error::new(
-                io::ErrorKind::Other,
-                format!("unexpected event in vm_stop: {:?}", e),
-            )),
+    fn wait_for_async_job(&mut self, job_id: &str) -> io::Result<()> {
+        let mut aborted = false;
+
+        'outer: loop {
+            self.qmp.nop()?;
+            for event in self.qmp.events() {
+                if let Event::JOB_STATUS_CHANGE { data, timestamp: _ } = event {
+                    if data.id != job_id {
+                        continue;
+                    }
+                    match data.status {
+                        JobStatus::created
+                        | JobStatus::ready
+                        | JobStatus::running
+                        | JobStatus::waiting
+                        | JobStatus::pending => (),
+                        JobStatus::aborting => {
+                            aborted = true;
+                        }
+                        JobStatus::concluded => break 'outer,
+                        JobStatus::standby
+                        | JobStatus::null
+                        | JobStatus::undefined
+                        | JobStatus::paused => {
+                            return Err(io::Error::new(
+                                io::ErrorKind::Other,
+                                format!("unexpected job status: {:?}", data.status),
+                            ));
+                        }
+                    }
+                }
+            }
+        }
+
+        // early return if job completed successfully
+        if !aborted {
+            return Ok(());
+        }
+
+        // probe for a reason
+        let mut reason = None;
+        for item in self.qmp.execute(&qmp::query_jobs {})? {
+            if item.id != job_id {
+                continue;
+            }
+            let error = item
+                .error
+                .unwrap_or_else(|| "aborted without an error message".to_string());
+            reason = Some(error);
+            break;
+        }
+
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!(
+                "job aborted: {}",
+                reason.unwrap_or_else(|| "unable to probe for a reason".to_string())
+            ),
+        ))
+    }
+
+    fn wait_for_status_change<F: Fn(Event) -> bool>(&mut self, predicate: F) -> io::Result<()> {
+        loop {
+            self.qmp.nop()?;
+            for event in self.qmp.events() {
+                if predicate(event) {
+                    return Ok(());
+                }
+            }
         }
     }
 
+    fn vm_stop(&mut self) -> io::Result<()> {
+        self.qmp.execute(&qmp::stop {})?;
+        self.wait_for_status_change(|event| matches!(event, Event::STOP { .. }))
+    }
+
     fn vm_cont(&mut self) -> io::Result<()> {
-        let (_, event) = exec_sync(&self.socket, qmp::cont {})?;
-        match event {
-            Some(Event::RESUME { .. }) => Ok(()),
-            e => Err(io::Error::new(
-                io::ErrorKind::Other,
-                format!("unexpected event in vm_cont: {:?}", e),
-            )),
-        }
+        self.qmp.execute(&qmp::cont {})?;
+        self.wait_for_status_change(|event| matches!(event, Event::RESUME { .. }))
     }
 
     pub fn snapshot_save(&mut self) -> io::Result<()> {
         self.vm_stop()?;
 
         let job_id = self.next_job_id();
-        exec_async(
-            &self.socket,
-            qmp::snapshot_save {
-                tag: SNAPSHOT_TAG.to_string(),
-                job_id: job_id.clone(),
-                vmstate: SNAPSHOT_DISK.to_string(),
-                devices: vec![SNAPSHOT_DISK.to_string()],
-            },
-            &job_id,
-        )
+        self.qmp.execute(&qmp::snapshot_save {
+            tag: SNAPSHOT_TAG.to_string(),
+            job_id: job_id.clone(),
+            vmstate: SNAPSHOT_DISK.to_string(),
+            devices: vec![SNAPSHOT_DISK.to_string()],
+        })?;
+        self.wait_for_async_job(&job_id)
     }
 
     pub fn snapshot_load(&mut self) -> io::Result<()> {
         let job_id = self.next_job_id();
-        exec_async(
-            &self.socket,
-            qmp::snapshot_load {
-                tag: SNAPSHOT_TAG.to_string(),
-                job_id: job_id.clone(),
-                vmstate: SNAPSHOT_DISK.to_string(),
-                devices: vec![SNAPSHOT_DISK.to_string()],
-            },
-            &job_id,
-        )?;
+        self.qmp.execute(&qmp::snapshot_load {
+            tag: SNAPSHOT_TAG.to_string(),
+            job_id: job_id.clone(),
+            vmstate: SNAPSHOT_DISK.to_string(),
+            devices: vec![SNAPSHOT_DISK.to_string()],
+        })?;
+        self.wait_for_async_job(&job_id)?;
 
         self.vm_cont()
     }
 
-    pub fn query_status(&mut self) -> io::Result<VMStatus> {
-        let (resp, _) = exec_sync(&self.socket, qmp::query_status {})?;
-        let status = match resp.status {
-            RunState::running => VMStatus::Running,
-            RunState::shutdown | RunState::guest_panicked => VMStatus::Stopped,
-            RunState::internal_error | RunState::io_error => VMStatus::Error,
-            RunState::debug
-            | RunState::suspended
-            | RunState::paused
-            | RunState::save_vm
-            | RunState::restore_vm
-            | RunState::prelaunch
-            | RunState::inmigrate
-            | RunState::postmigrate
-            | RunState::finish_migrate
-            | RunState::watchdog
-            | RunState::colo => {
-                return Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    format!("unrecognized run state: {:?}", resp.status),
-                ));
+    pub fn wait_for_guest_finish(&mut self) -> io::Result<VMExitMode> {
+        let mut mode = VMExitMode::Soft;
+        loop {
+            self.qmp.nop()?;
+            for event in self.qmp.events() {
+                match &event {
+                    Event::STOP { .. } => return Ok(mode),
+                    Event::SHUTDOWN { data, .. } => {
+                        if !data.guest {
+                            mode = VMExitMode::Hard;
+                        }
+                    }
+                    Event::POWERDOWN { .. } => {
+                        mode = VMExitMode::Hard;
+                    }
+                    Event::RESET { data, .. } => {
+                        if !data.guest {
+                            mode = VMExitMode::Hard;
+                        }
+                    }
+                    Event::GUEST_PANICKED { .. } => {}
+                    Event::MEMORY_FAILURE { .. }
+                    | Event::BLOCK_IMAGE_CORRUPTED { .. }
+                    | Event::BLOCK_IO_ERROR { .. }
+                    | Event::BLOCK_JOB_ERROR { .. } => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::Other,
+                            format!("unexpected error detected in event stream: {:?}", event),
+                        ));
+                    }
+                    _ => (),
+                }
             }
-        };
-        Ok(status)
+        }
     }
 }
 
-pub enum VMStatus {
-    Running,
-    Stopped,
-    Error,
+pub enum VMExitMode {
+    Soft,
+    Hard,
 }
