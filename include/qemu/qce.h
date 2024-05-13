2b86783624bba8ba8c4d208501e401c294291dfc
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-05-13 15:46:14 -0400

    x

--- include/qemu/qce.h
+++ include/qemu/qce.h
@@ -2,6 +2,7 @@
 #define QEMU_QCE_H
 
 #include "hw/core/cpu.h"
+#include "qemu/thread.h"
 
 // logging utilities
 #ifndef QCE_RELEASE
@@ -16,6 +17,11 @@
 // command selector
 #define SGX_EDBGWR 0x05
 
+// context
+struct QCEContext {
+  QemuSpin lock;
+};
+
 // initialize the context
 static inline int qce_init(CPUState *cpu) {
   // repurpose the kvm_state (which is only used in kvm) for context
@@ -23,6 +29,17 @@ static inline int qce_init(CPUState *cpu) {
     qce_fatal("kvm_state is not NULL, cannot repurpose it");
     return 1;
   }
+
+  // initialize the context
+  struct QCEContext *ctxt = malloc(sizeof(struct QCEContext));
+  if (ctxt == NULL) {
+    qce_fatal("unable to allocate QCE context");
+    return 1;
+  }
+  qemu_spin_init(&ctxt->lock);
+
+  // done
+  cpu->kvm_state = (struct KVMState *)ctxt;
   return 0;
 }
 
@@ -32,6 +49,9 @@ static inline void qce_shutdown(CPUState *cpu) {
   if (cpu->kvm_state == NULL) {
     return;
   }
+
+  // free memory usage
+  free(cpu->kvm_state);
 }
 
 #endif /* QEMU_QCE_H */
