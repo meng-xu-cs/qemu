1faee36494749d349e4988a350eeb4b7c459beaf
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-05-17 23:14:58 -0400

    x

--- tcg/qce-ir.h
+++ tcg/qce-ir.h
@@ -1,14 +1,32 @@
+struct QCEVar {
+  TCGTempKind kind;
+  union {
+    // kind: TEMP_FIXED
+    struct {
+      TCGReg reg : 8;
+    } v_fixed;
+
+    // kind: TEMP_GLOBAL
+    struct {
+    } v_global_direct;
+    struct {
+    } v_global_indirect;
+  };
+};
+
+static inline void parse_temp(const TCGTemp *t) {}
+
 struct QCEInst {
   TCGOpcode opc;
   union {
-    // op: discard
+    // opc: discard
     struct {
       TCGArg out;
     } op_discard;
   } inst;
 };
 
-static inline void parse(const TCGOp *op, struct QCEInst *inst) {
+static inline void parse_op(const TCGOp *op, struct QCEInst *inst) {
   switch (op->opc) {
   case INDEX_op_discard:
     return;
