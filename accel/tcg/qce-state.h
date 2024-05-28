a9c7292ec4592a4e489c2dc3207cbc5f2474eef6
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-05-28 19:53:52 -0400

    x

--- accel/tcg/qce-state.h
+++ accel/tcg/qce-state.h
@@ -7,37 +7,64 @@
 
 #define QCE_EXPR_BASE_SIZE sizeof(uint32_t)
 
+typedef enum {
+  QCE_EXPR_CONCRETE,
+  QCE_EXPR_SYMBOLIC,
+} QCEExprMode;
+
+// dual-mode holder for expressions
+typedef struct {
+  GTree *dispatch;
+  GTree *symbolic;
+  GTree *concrete;
+} QCEExprHolder;
+
+gint qce_gtree_addr_key_cmp(gconstpointer a, gconstpointer b);
+gint qce_gtree_addr_key_cmp(gconstpointer a, gconstpointer b) {
+  return (uintptr_t)a - (uintptr_t)b;
+}
+
+static inline void qce_expr_holder_init(QCEExprHolder *holder) {
+  holder->dispatch = g_tree_new(qce_gtree_addr_key_cmp);
+  holder->symbolic = g_tree_new(qce_gtree_addr_key_cmp);
+  holder->concrete = g_tree_new(qce_gtree_addr_key_cmp);
+}
+
+static inline void qce_expr_holder_fini(QCEExprHolder *holder) {
+  g_tree_destroy(holder->dispatch);
+  g_tree_destroy(holder->symbolic);
+  g_tree_destroy(holder->concrete);
+}
+
+static inline void qce_expr_holder_put_symbolic(QCEExprHolder *holder,
+                                                uintptr_t offset, Z3_ast expr) {
+  gpointer key = (gpointer)offset;
+  g_tree_insert(holder->dispatch, key, (gpointer)QCE_EXPR_SYMBOLIC);
+  g_tree_insert(holder->symbolic, key, expr);
+}
+
 // dual-mode representation of the machine state
 typedef struct {
   // z3 context
   SolverZ3 solver_z3;
 
-  // env (on host)
-  QTree *env_symbolic;
-  QTree *env_concrete;
-  // mem (on guest)
-  QTree *mem_symbolic;
-  QTree *mem_concrete;
+  // expressions
+  QCEExprHolder env; // on host
+  QCEExprHolder mem; // on guest
+  QCEExprHolder tmp; // in IR
 } QCEState;
-
-gint qce_qtree_addr_key_cmp(gconstpointer a, gconstpointer b);
-gint qce_qtree_addr_key_cmp(gconstpointer a, gconstpointer b) {
-  return (uintptr_t)a - (uintptr_t)b;
-}
 
 static inline void qce_state_init(QCEState *state) {
   qce_smt_z3_init(&state->solver_z3);
-  state->env_symbolic = q_tree_new(qce_qtree_addr_key_cmp);
-  state->env_concrete = q_tree_new(qce_qtree_addr_key_cmp);
-  state->mem_symbolic = q_tree_new(qce_qtree_addr_key_cmp);
-  state->mem_concrete = q_tree_new(qce_qtree_addr_key_cmp);
+  qce_expr_holder_init(&state->env);
+  qce_expr_holder_init(&state->mem);
+  qce_expr_holder_init(&state->tmp);
 }
 
 static inline void qce_state_fini(QCEState *state) {
-  q_tree_destroy(state->env_symbolic);
-  q_tree_destroy(state->env_concrete);
-  q_tree_destroy(state->mem_symbolic);
-  q_tree_destroy(state->mem_concrete);
+  qce_expr_holder_fini(&state->env);
+  qce_expr_holder_fini(&state->mem);
+  qce_expr_holder_fini(&state->tmp);
   qce_smt_z3_fini(&state->solver_z3);
 }
 
@@ -46,8 +73,7 @@ qce_state_env_put_symbolic_i32(QCEState *state, uintptr_t offset, Z3_ast expr) {
   if (offset % QCE_EXPR_BASE_SIZE != 0) {
     qce_fatal("misaligned symbolic mark");
   }
-  q_tree_remove(state->env_concrete, (gpointer)offset);
-  q_tree_insert(state->env_symbolic, (gpointer)offset, expr);
+  qce_expr_holder_put_symbolic(&state->env, offset, expr);
 }
 
 static inline void
@@ -58,13 +84,11 @@ qce_state_env_put_symbolic_i64(QCEState *state, uintptr_t offset, Z3_ast expr) {
 
   uintptr_t offset_t = offset;
   Z3_ast expr_t = qce_smt_z3_bv64_extract_t(&state->solver_z3, expr);
-  q_tree_remove(state->env_concrete, (gpointer)offset_t);
-  q_tree_insert(state->env_symbolic, (gpointer)offset_t, expr_t);
+  qce_expr_holder_put_symbolic(&state->env, offset_t, expr_t);
 
   uintptr_t offset_b = offset + QCE_EXPR_BASE_SIZE;
   Z3_ast expr_b = qce_smt_z3_bv64_extract_b(&state->solver_z3, expr);
-  q_tree_remove(state->env_concrete, (gpointer)offset_b);
-  q_tree_insert(state->env_symbolic, (gpointer)offset_b, expr_b);
+  qce_expr_holder_put_symbolic(&state->env, offset_b, expr_b);
 }
 
 #endif // QEMU_QCE_STATE_H
