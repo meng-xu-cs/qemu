6e00a6097bd713a3b02a35ba62c2fbe3edf0d0bb
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-05-30 22:59:53 -0400

    x

--- accel/tcg/qce-state.h
+++ accel/tcg/qce-state.h
@@ -4,8 +4,9 @@
 
 #define QCE_CELL_SIZE sizeof(int32_t)
 
-// if mask == 0x00, the expr is a symbolic expr
-// if mask == 0xff, the expr is a concrete value
+// if symbolic == NULL (which means mask == 0x00), the expr is null
+// if mask == 0x00, the expr is symbolic
+// if mask == 0xff, the expr is concrete
 // invalid in all other cases
 typedef union {
   struct __attribute__((__packed__)) {
@@ -17,14 +18,22 @@ typedef union {
 } QCECell;
 static_assert(sizeof(QCECell) == sizeof(gpointer));
 
-static inline bool qce_cell_is_concrete(const QCECell cell) {
+typedef enum {
+  QCE_CELL_NULL,
+  QCE_CELL_CONCRETE,
+  QCE_CELL_SYMBOLIC,
+} QCECellMode;
+
+static inline QCECellMode qce_cell_mode(const QCECell cell) {
+  if (cell.symbolic == NULL) {
+    return QCE_CELL_NULL;
+  }
   const uint8_t v = cell.mask & 0xff;
   if (v == 0xff) {
-    return true;
-  } else {
-    assert(v == 0);
-    return false;
+    return QCE_CELL_CONCRETE;
   }
+  assert(v == 0);
+  return QCE_CELL_SYMBOLIC;
 }
 
 // dual-mode holder for expressions
@@ -34,7 +43,7 @@ typedef struct {
 
 gint qce_gtree_addr_key_cmp(gconstpointer a, gconstpointer b);
 gint qce_gtree_addr_key_cmp(gconstpointer a, gconstpointer b) {
-  return (uintptr_t)a - (uintptr_t)b;
+  return (intptr_t)a - (intptr_t)b;
 }
 
 static inline void qce_cell_holder_init(QCECellHolder *holder) {
@@ -105,7 +114,7 @@ static inline void qce_state_fini(QCEState *state) {
 }
 
 static inline void
-qce_state_env_put_concrete_i32(QCEState *state, uintptr_t offset, int32_t val) {
+qce_state_env_put_concrete_i32(QCEState *state, intptr_t offset, int32_t val) {
   if (offset % QCE_CELL_SIZE != 0) {
     qce_fatal("misaligned offset for env location");
   }
@@ -113,18 +122,21 @@ qce_state_env_put_concrete_i32(QCEState *state, uintptr_t offset, int32_t val) {
 }
 
 static inline void
-qce_state_env_put_concrete_i64(QCEState *state, uintptr_t offset, int64_t val) {
+qce_state_env_put_concrete_i64(QCEState *state, intptr_t offset, int64_t val) {
   if (offset % QCE_CELL_SIZE != 0) {
     qce_fatal("misaligned offset for env location");
   }
-
   int32_t *cell = (int32_t *)&val;
+
   gpointer key_t = (gpointer)offset;
-  qce_cell_holder_put_concrete(&state->env, key_t, *cell);
+  qce_cell_holder_put_concrete(&state->env, key_t, cell[0]);
+
+  gpointer key_b = (gpointer)(offset + QCE_CELL_SIZE);
+  qce_cell_holder_put_concrete(&state->env, key_b, cell[1]);
 }
 
 static inline void
-qce_state_env_put_symbolic_i32(QCEState *state, uintptr_t offset, Z3_ast expr) {
+qce_state_env_put_symbolic_i32(QCEState *state, intptr_t offset, Z3_ast expr) {
   if (offset % QCE_CELL_SIZE != 0) {
     qce_fatal("misaligned offset for env location");
   }
@@ -132,7 +144,7 @@ qce_state_env_put_symbolic_i32(QCEState *state, uintptr_t offset, Z3_ast expr) {
 }
 
 static inline void
-qce_state_env_put_symbolic_i64(QCEState *state, uintptr_t offset, Z3_ast expr) {
+qce_state_env_put_symbolic_i64(QCEState *state, intptr_t offset, Z3_ast expr) {
   if (offset % QCE_CELL_SIZE != 0) {
     qce_fatal("misaligned offset for env location");
   }
@@ -146,58 +158,127 @@ qce_state_env_put_symbolic_i64(QCEState *state, uintptr_t offset, Z3_ast expr) {
   qce_cell_holder_put_symbolic(&state->env, key_b, expr_b);
 }
 
-static inline void qce_state_env_get_i32(QCEState *state, uintptr_t offset,
-                                         QCEExpr *expr) {
+static inline void qce_state_env_get_i32(CPUArchState *env, QCEState *state,
+                                         intptr_t offset, QCEExpr *expr) {
   if (offset % QCE_CELL_SIZE != 0) {
     qce_fatal("misaligned offset for env location");
   }
 
   const QCECell cell = qce_cell_holder_get(&state->env, (gpointer)offset);
-  if (qce_cell_is_concrete(cell)) {
+  switch (qce_cell_mode(cell)) {
+  case QCE_CELL_NULL: {
+    expr->mode = QCE_EXPR_CONCRETE;
+    expr->v_i32 = *(int32_t *)((intptr_t)env + offset);
+    break;
+  }
+  case QCE_CELL_CONCRETE: {
     expr->mode = QCE_EXPR_CONCRETE;
     expr->v_i32 = cell.concrete;
-  } else {
+    break;
+  }
+  case QCE_CELL_SYMBOLIC: {
     expr->mode = QCE_EXPR_SYMBOLIC;
     expr->symbolic = cell.symbolic;
+    break;
   }
+  }
+
   expr->type = QCE_EXPR_I32;
 }
 
-static inline void qce_state_env_get_i64(QCEState *state, uintptr_t offset,
-                                         QCEExpr *expr) {
+static inline void qce_state_env_get_i64(CPUArchState *env, QCEState *state,
+                                         intptr_t offset, QCEExpr *expr) {
   if (offset % QCE_CELL_SIZE != 0) {
     qce_fatal("misaligned offset for env location");
   }
 
   gpointer key_t = (gpointer)offset;
   const QCECell cell_t = qce_cell_holder_get(&state->env, key_t);
+  const QCECellMode mode_t = qce_cell_mode(cell_t);
 
   gpointer key_b = (gpointer)(offset + QCE_CELL_SIZE);
   const QCECell cell_b = qce_cell_holder_get(&state->env, key_b);
+  const QCECellMode mode_b = qce_cell_mode(cell_b);
 
-  if (qce_cell_is_concrete(cell_t)) {
-    if (qce_cell_is_concrete(cell_b)) {
+  switch (mode_t) {
+  case QCE_CELL_NULL: {
+    switch (mode_b) {
+    case QCE_CELL_NULL: {
       expr->mode = QCE_EXPR_CONCRETE;
-      expr->v_i64 = (int64_t)cell_t.concrete << 32 | cell_b.concrete;
-    } else {
+      expr->v_i64 = *(int64_t *)((intptr_t)env + offset);
+      break;
+    }
+    case QCE_CELL_CONCRETE: {
+      int32_t val_t = *(int32_t *)((intptr_t)env + offset);
+      expr->mode = QCE_EXPR_CONCRETE;
+      expr->v_i64 = (int64_t)val_t << 32 | cell_b.concrete;
+      break;
+    }
+    case QCE_CELL_SYMBOLIC: {
+      int32_t val_t = *(int32_t *)((intptr_t)env + offset);
+      Z3_ast expr_t = qce_smt_z3_bv32_value(&state->solver_z3, val_t);
       expr->mode = QCE_EXPR_SYMBOLIC;
-      Z3_ast expr_t = qce_smt_z3_bv32_value(&state->solver_z3, cell_t.concrete);
       expr->symbolic =
           qce_smt_z3_bv64_concat(&state->solver_z3, expr_t, cell_b.symbolic);
+      break;
     }
-  } else {
-    expr->mode = QCE_EXPR_SYMBOLIC;
-    Z3_ast expr_b;
-    if (qce_cell_is_concrete(cell_b)) {
-      expr_b = qce_smt_z3_bv32_value(&state->solver_z3, cell_b.concrete);
-    } else {
-      expr_b = cell_b.symbolic;
     }
-    expr->symbolic =
-        qce_smt_z3_bv64_concat(&state->solver_z3, cell_t.symbolic, expr_b);
+    break;
   }
+  case QCE_CELL_CONCRETE: {
+    switch (mode_b) {
+    case QCE_CELL_NULL: {
+      int32_t val_b = *(int32_t *)((intptr_t)env + offset + QCE_CELL_SIZE);
+      expr->mode = QCE_EXPR_CONCRETE;
+      expr->v_i64 = (int64_t)cell_t.concrete << 32 | val_b;
+      break;
+    }
+    case QCE_CELL_CONCRETE: {
+      expr->mode = QCE_EXPR_CONCRETE;
+      expr->v_i64 = (int64_t)cell_t.concrete << 32 | cell_b.concrete;
+      break;
+    }
+    case QCE_CELL_SYMBOLIC: {
+      Z3_ast expr_t = qce_smt_z3_bv32_value(&state->solver_z3, cell_t.concrete);
+      expr->mode = QCE_EXPR_SYMBOLIC;
+      expr->symbolic =
+          qce_smt_z3_bv64_concat(&state->solver_z3, expr_t, cell_b.symbolic);
+      break;
+    }
+    }
+    break;
+  }
+  case QCE_CELL_SYMBOLIC: {
+    switch (mode_b) {
+    case QCE_CELL_NULL: {
+      int32_t val_b = *(int32_t *)((intptr_t)env + offset + QCE_CELL_SIZE);
+      Z3_ast expr_b = qce_smt_z3_bv32_value(&state->solver_z3, val_b);
+      expr->mode = QCE_EXPR_SYMBOLIC;
+      expr->symbolic =
+          qce_smt_z3_bv64_concat(&state->solver_z3, cell_t.symbolic, expr_b);
+      break;
+    }
+    case QCE_CELL_CONCRETE: {
+      Z3_ast expr_b = qce_smt_z3_bv32_value(&state->solver_z3, cell_b.concrete);
+      expr->mode = QCE_EXPR_SYMBOLIC;
+      expr->symbolic =
+          qce_smt_z3_bv64_concat(&state->solver_z3, cell_t.symbolic, expr_b);
+      break;
+    }
+    case QCE_CELL_SYMBOLIC: {
+      expr->mode = QCE_EXPR_SYMBOLIC;
+      expr->symbolic = qce_smt_z3_bv64_concat(&state->solver_z3,
+                                              cell_t.symbolic, cell_b.symbolic);
+      break;
+    }
+    }
+    break;
+  }
+  }
+
   expr->type = QCE_EXPR_I64;
 }
+
 static inline void qce_state_get_var(CPUArchState *env, QCEState *state,
                                      QCEVar *var, QCEExpr *expr) {
   // TODO
@@ -205,31 +286,68 @@ static inline void qce_state_get_var(CPUArchState *env, QCEState *state,
   case QCE_VAR_CONST: {
     expr->mode = QCE_EXPR_CONCRETE;
     switch (var->type) {
-    case TCG_TYPE_I32:
+    case TCG_TYPE_I32: {
       expr->type = QCE_EXPR_I32;
       expr->v_i32 = var->v_const.val;
       break;
-    case TCG_TYPE_I64:
+    }
+    case TCG_TYPE_I64: {
       expr->type = QCE_EXPR_I64;
       expr->v_i64 = var->v_const.val;
       break;
+    }
     default:
-      qce_fatal("invalid QCE variable type");
-      break;
+      qce_fatal("invalid QCE variable type for const");
     }
     break;
   }
   case QCE_VAR_FIXED: {
+#ifdef QCE_DEBUG_IR
+    if (var->type != TCG_TYPE_I64) {
+      qce_fatal("invalid QCE variable type for fixed");
+    }
+#endif
     expr->mode = QCE_EXPR_CONCRETE;
     expr->type = QCE_EXPR_I64;
     expr->v_i64 = (int64_t)env;
     break;
   }
   case QCE_VAR_GLOBAL_DIRECT: {
+    switch (var->type) {
+    case TCG_TYPE_I32: {
+      qce_state_env_get_i32(env, state, var->v_global_direct.offset, expr);
+      break;
+    }
+    case TCG_TYPE_I64: {
+      qce_state_env_get_i64(env, state, var->v_global_direct.offset, expr);
+      break;
+    }
+    default:
+      qce_fatal("invalid QCE variable type for direct_global");
+    }
     break;
   }
-  case QCE_VAR_GLOBAL_INDIRECT:
+  case QCE_VAR_GLOBAL_INDIRECT: {
+    switch (var->type) {
+    case TCG_TYPE_I32: {
+      qce_state_env_get_i32(env, state,
+                            var->v_global_indirect.offset1 +
+                                var->v_global_indirect.offset2,
+                            expr);
+      break;
+    }
+    case TCG_TYPE_I64: {
+      qce_state_env_get_i64(env, state,
+                            var->v_global_indirect.offset1 +
+                                var->v_global_indirect.offset2,
+                            expr);
+      break;
+    }
+    default:
+      qce_fatal("invalid QCE variable type for direct_global");
+    }
     break;
+  }
   case QCE_VAR_TB:
     break;
   case QCE_VAR_EBB:
