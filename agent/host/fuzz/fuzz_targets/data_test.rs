9dae24254930fab7d66ea5bff4b2d7fbfd1f528e
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-06-03 21:53:20 -0400

    formatting

--- agent/host/fuzz/fuzz_targets/data_test.rs
+++ agent/host/fuzz/fuzz_targets/data_test.rs
@@ -2,19 +2,48 @@
 
 use libfuzzer_sys::{fuzz_mutator, fuzz_target, fuzzz_crossover};
 
-extern "C" { pub fn SubmitLibfuzzerCoverage(pc: u64); } 
-extern "C" { pub fn ProtoFuzzerInputOne(data: *const u8, size: usize, out: *mut u8, max_out_size: usize) -> usize; }
-extern "C" { pub fn ProtoFuzzerCustomMutator(data: *mut u8, size: usize, max_size: usize, seed: u32) -> usize; }
-extern "C" { pub fn ProtoFuzzerCustomCrossover(data1: *const u8, size1: usize, data2: *const u8, size2: usize, out: *mut u8, max_out_size: usize, seed: u32) -> usize; }
+extern "C" {
+    pub fn SubmitLibfuzzerCoverage(pc: u64);
+}
+extern "C" {
+    pub fn ProtoFuzzerInputOne(
+        data: *const u8,
+        size: usize,
+        out: *mut u8,
+        max_out_size: usize,
+    ) -> usize;
+}
+extern "C" {
+    pub fn ProtoFuzzerCustomMutator(
+        data: *mut u8,
+        size: usize,
+        max_size: usize,
+        seed: u32,
+    ) -> usize;
+}
+extern "C" {
+    pub fn ProtoFuzzerCustomCrossover(
+        data1: *const u8,
+        size1: usize,
+        data2: *const u8,
+        size2: usize,
+        out: *mut u8,
+        max_out_size: usize,
+        seed: u32,
+    ) -> usize;
+}
 
 fuzz_target!(|data: &[u8]| {
     // output fuzz data
     let mut out = vec![0u8; 1048576];
-    let out_size = unsafe { ProtoFuzzerInputOne(data.as_ptr(), data.len(), out.as_mut_ptr(), out.len()) };
+    let out_size =
+        unsafe { ProtoFuzzerInputOne(data.as_ptr(), data.len(), out.as_mut_ptr(), out.len()) };
     // println!("out_size: {}", out_size);
     // println!("out: {:?}", &out[..out_size]);
     // submit coverage
-    unsafe { SubmitLibfuzzerCoverage(out_size as u64); }
+    unsafe {
+        SubmitLibfuzzerCoverage(out_size as u64);
+    }
 });
 
 fuzz_mutator!(
@@ -27,6 +56,14 @@ fuzz_mutator!(
 
 fuzz_crossover!(|data1: &[u8], data2: &[u8], out: &mut [u8], seed: u32| {
     unsafe {
-        return ProtoFuzzerCustomCrossover(data1.as_ptr(), data1.len(), data2.as_ptr(), data2.len(), out.as_mut_ptr(), out.len(), seed);
+        return ProtoFuzzerCustomCrossover(
+            data1.as_ptr(),
+            data1.len(),
+            data2.as_ptr(),
+            data2.len(),
+            out.as_mut_ptr(),
+            out.len(),
+            seed,
+        );
     }
 });
--- agent/host/fuzz/fuzz_targets/fuzz_target_1.rs
+++ agent/host/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,6 +1,6 @@
 #![no_main]
 
-use libfuzzer_sys::{fuzz_mutator, fuzz_target, fuzz_crossover};
+use libfuzzer_sys::{fuzz_crossover, fuzz_mutator, fuzz_target};
 use std::os::unix::net::UnixStream;
 use std::path::PathBuf;
 
@@ -21,19 +21,45 @@ static mut VMIO: Option<&mut Vmio> = None;
 static mut QEMU: Option<QemuProxy> = None;
 static mut IVSHMEM: Option<Ivshmem> = None;
 
-extern "C" { pub fn SubmitLibfuzzerCoverage(pc: u64); } 
-extern "C" { pub fn ProtoFuzzerInputOne(data: *const u8, size: usize, out: *mut u8, max_out_size: usize) -> usize; }
-extern "C" { pub fn ProtoFuzzerCustomMutator(data: *mut u8, size: usize, max_size: usize, seed: u32) -> usize; }
-extern "C" { pub fn ProtoFuzzerCustomCrossover(data1: *const u8, size1: usize, data2: *const u8, size2: usize, out: *mut u8, max_out_size: usize, seed: u32) -> usize; }
+extern "C" {
+    pub fn SubmitLibfuzzerCoverage(pc: u64);
+}
+extern "C" {
+    pub fn ProtoFuzzerInputOne(
+        data: *const u8,
+        size: usize,
+        out: *mut u8,
+        max_out_size: usize,
+    ) -> usize;
+}
+extern "C" {
+    pub fn ProtoFuzzerCustomMutator(
+        data: *mut u8,
+        size: usize,
+        max_size: usize,
+        seed: u32,
+    ) -> usize;
+}
+extern "C" {
+    pub fn ProtoFuzzerCustomCrossover(
+        data1: *const u8,
+        size1: usize,
+        data2: *const u8,
+        size2: usize,
+        out: *mut u8,
+        max_out_size: usize,
+        seed: u32,
+    ) -> usize;
+}
 
 fuzz_target!(|data: &[u8]| {
-
     unsafe {
         if !INITED {
             // fuzzed code goes here
             // let args = Options::from_args();
             let verbose = env::var("AIXCC_KERNEL_FUZZ_VERBOSE").is_ok();
-            let path_tmp_var = env::var("AIXCC_KERNEL_FUZZ_TMP").unwrap_or_else(|_| "/tmp/aixcc_kernel_fuzz".to_string());
+            let path_tmp_var = env::var("AIXCC_KERNEL_FUZZ_TMP")
+                .unwrap_or_else(|_| "/tmp/aixcc_kernel_fuzz".to_string());
             let path_tmp = PathBuf::from(path_tmp_var);
             env_logger::builder()
                 .format_timestamp(None)
@@ -53,11 +79,13 @@ fuzz_target!(|data: &[u8]| {
             info!("QEMU is up and running");
 
             let path_ivshmem = path_tmp.join(VM_IVSHMEM_FILE);
-            IVSHMEM = Some(Ivshmem::new(&path_ivshmem, VM_IVSHMEM_SIZE)
-                .unwrap_or_else(|e| panic!("error mapping ivshmem: {}", e)));
+            IVSHMEM = Some(
+                Ivshmem::new(&path_ivshmem, VM_IVSHMEM_SIZE)
+                    .unwrap_or_else(|e| panic!("error mapping ivshmem: {}", e)),
+            );
             let ivshmem = IVSHMEM.as_mut().unwrap();
 
-            VMIO = Some(ivshmem.vmio()); 
+            VMIO = Some(ivshmem.vmio());
             let vmio = VMIO.as_deref_mut().unwrap();
             vmio.init()
                 .unwrap_or_else(|e| panic!("error initializing vmio: {}", e));
@@ -65,10 +93,14 @@ fuzz_target!(|data: &[u8]| {
 
             // connect to QEMU monitor
             let path_monitor_socket = path_tmp.join(VM_MONITOR_SOCKET);
-            let stream = Box::new(UnixStream::connect(path_monitor_socket)
-                .unwrap_or_else(|e| panic!("error connecting to QEMU monitor socket: {}", e)));
-            QEMU = Some(QemuProxy::new(Box::leak(stream))
-                .unwrap_or_else(|e| panic!("error negotiating with the QEMU-QMP: {}", e)));
+            let stream = Box::new(
+                UnixStream::connect(path_monitor_socket)
+                    .unwrap_or_else(|e| panic!("error connecting to QEMU monitor socket: {}", e)),
+            );
+            QEMU = Some(
+                QemuProxy::new(Box::leak(stream))
+                    .unwrap_or_else(|e| panic!("error negotiating with the QEMU-QMP: {}", e)),
+            );
             info!("QEMU-QMP connected");
             let qemu = QEMU.as_mut().unwrap();
 
@@ -83,67 +115,63 @@ fuzz_target!(|data: &[u8]| {
             INITED = true;
         }
 
-    // invoke data feeder 
-    let mut out = vec![0u8; 1048576];
-    let out_size = ProtoFuzzerInputOne(data.as_ptr(), data.len(), out.as_mut_ptr(), out.len());
-    // println!("out_size: {}", out_size);
-    // println!("out: {:?}", &out[..out_size]);
-    if out_size == 0 {
-        return;
-    }
-    
+        // invoke data feeder
+        let mut out = vec![0u8; 1048576];
+        let out_size = ProtoFuzzerInputOne(data.as_ptr(), data.len(), out.as_mut_ptr(), out.len());
+        // println!("out_size: {}", out_size);
+        // println!("out: {:?}", &out[..out_size]);
+        if out_size == 0 {
+            return;
+        }
 
-    // fuzzing loop
-    // release the guest
-    let vmio = VMIO.as_deref_mut().unwrap();
+        // fuzzing loop
+        // release the guest
+        let vmio = VMIO.as_deref_mut().unwrap();
 
+        // send data to guest
+        out.truncate(out_size);
+        vmio.send_fuzz_input(&out, out_size);
 
-    // send data to guest
-    out.truncate(out_size);
-    vmio.send_fuzz_input(&out, out_size);
+        // debug
+        // info!("fuzz input sent to guest: {:?}", out);
 
-    // debug 
-    // info!("fuzz input sent to guest: {:?}", out);
+        // TODO: use FFI to convert proto data -> raw data
 
-    // TODO: use FFI to convert proto data -> raw data
+        // vmio.post_to_guest();
+        info!("notified guest agent to continue");
 
-    // vmio.post_to_guest();
-    info!("notified guest agent to continue");
+        // wait for guest to stop
+        let qemu = QEMU.as_mut().unwrap();
+        match qemu
+            .wait_for_guest_reset()
+            .unwrap_or_else(|e| panic!("error waiting for status events from VM: {}", e))
+        {
+            VMExitMode::Soft => (),
+            VMExitMode::Hard | VMExitMode::Host => panic!("guest vm stopped"),
+        }
+        info!("guest vm stopped");
 
-    // wait for guest to stop
-    let qemu = QEMU.as_mut().unwrap();
-    match qemu
-        .wait_for_guest_reset()
-        .unwrap_or_else(|e| panic!("error waiting for status events from VM: {}", e))
-    {
-        VMExitMode::Soft => (),
-        VMExitMode::Hard | VMExitMode::Host => panic!("guest vm stopped"),
-    }
-    info!("guest vm stopped");
+        if !vmio.check_success() {
+            // dump fuzz input
+            println!("crash input: {:?}", out);
+            panic!("guest vm is forced to exit in an unexpected way");
+        }
 
-    if !vmio.check_success() {
-        // dump fuzz input
-        println!("crash input: {:?}", out);
-        panic!("guest vm is forced to exit in an unexpected way");
-    }
+        // get kcov info from vm
+        let kcov_info = vmio.get_kcov_info();
 
-    // get kcov info from vm 
-    let kcov_info = vmio
-        .get_kcov_info();
+        for i in 0..kcov_info.len() {
+            // submit kcov to libfuzzer
+            SubmitLibfuzzerCoverage(kcov_info[i] as u64);
+        }
 
-    for i in 0..kcov_info.len() {
-        // submit kcov to libfuzzer
-        SubmitLibfuzzerCoverage(kcov_info[i] as u64); 
-    }
+        // debug: output kcov array, as hex array
+        // info!("kcov info: {:?}", kcov_info);
 
-    // debug: output kcov array, as hex array
-    // info!("kcov info: {:?}", kcov_info);
-
-    // always refresh from a new snapshot
-    qemu.snapshot_load()
-        .unwrap_or_else(|e| panic!("error restoring a snapshot: {}", e));
-    info!("snapshot reloaded");
-
+        // always refresh from a new snapshot
+        qemu.snapshot_load()
+            .unwrap_or_else(|e| panic!("error restoring a snapshot: {}", e));
+        info!("snapshot reloaded");
     }
     // }
 
@@ -153,7 +181,6 @@ fuzz_target!(|data: &[u8]| {
     // drop the ivshmem at the end
     // drop(ivshmem);
 });
-
 
 fuzz_mutator!(
     |data: &mut [u8], size: usize, max_size: usize, _seed: u32| {
@@ -166,7 +193,14 @@ fuzz_mutator!(
 
 fuzz_crossover!(|data1: &[u8], data2: &[u8], out: &mut [u8], seed: u32| {
     unsafe {
-        return ProtoFuzzerCustomCrossover(data1.as_ptr(), data1.len(), data2.as_ptr(), data2.len(), out.as_mut_ptr(), out.len(), seed);
+        return ProtoFuzzerCustomCrossover(
+            data1.as_ptr(),
+            data1.len(),
+            data2.as_ptr(),
+            data2.len(),
+            out.as_mut_ptr(),
+            out.len(),
+            seed,
+        );
     }
 });
-
