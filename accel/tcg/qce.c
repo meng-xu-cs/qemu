1bad4ddb1e6ea85ddeaadb63e0c071df5875515c
Liyi Zhang <l392zhan@uwaterloo.ca>
2025-03-27 12:58:55 -0400

    fix bug in goto_tb

--- accel/tcg/qce.c
+++ accel/tcg/qce.c
@@ -650,7 +650,22 @@ void qce_on_tcg_tb_executed(TranslationBlock *tb, CPUState *cpu) {
         // TODO: validate this understanding
         break;
       }
-      goto end_of_loop;
+      /*
+	   * If a validated TB has been assigned to goto_tb, we need to manually
+       * execute the TB since QEMU will not go back to the main loop and
+	   * invoke qce_on_tcg_tb_executed until all the TBs in the chaining have
+       * been executed.
+	   * Otherwise, just simply return and let QEMU dynamically decide which
+	   * TB to be executed next.
+	   */
+      g_assert(!(((TranslationBlock *)next_tb)->cflags & CF_INVALID));
+#ifdef QCE_DEBUG_IR
+	  if (g_qce->trace_file != NULL) {
+		fprintf(g_qce->trace_file, "<<<<\n");
+	  }
+#endif
+	  qce_on_tcg_tb_executed((TranslationBlock *)next_tb, cpu);
+      return;
     }
     case QCE_INST_EXIT_TB: {
       // TODO: what about the return value? i.e., inst->i_exit_tb.idx
